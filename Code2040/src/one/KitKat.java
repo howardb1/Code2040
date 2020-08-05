4eb41ca6c946422cfa22136a6893f5fa5aa69557
King_Chop27$ <howardb1@wit.edu>
2020-08-05 01:50:04 -0400

    First Commit. Uploading all the files

---
+++ Code2040/src/one/KitKat.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package one;
+
+/**
+ * @author brianhoward
+ *
+ */
+public class KitKat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Splat");
+		
+		
+	}
+
+}
