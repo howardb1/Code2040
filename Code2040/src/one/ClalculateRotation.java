418055a1939879bd2cc27847402084f28ad51713
King_Chop27$ <howardb1@wit.edu>
2020-08-07 02:46:56 -0400

    Second push

---
+++ Code2040/src/one/ClalculateRotation.java
@@ -0,0 +1,37 @@
+/**
+ * @author brianhoward
+ */
+package one;
+import java.util.Scanner;
+
+
+public class ClalculateRotation {
+
+	@SuppressWarnings({ "null", "unused" })
+	public static int shiftedDiff(String first, String second) {
+		
+	for(int i = 0; i< first.length(); i++) {
+		for (int j = 0; i<second.length();j++) {
+			return (Integer) null;
+		}
+	}
+		
+	return -1;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		
+		
+		
+		
+		
+		
+	}
+
+}
