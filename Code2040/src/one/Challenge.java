ed1798df0e0be15e0423f84820957c1cf320b5d6
King_Chop27$ <howardb1@wit.edu>
2020-08-13 05:52:12 -0400

    Second "challenge" push

--- Code2040/src/one/Challenge.java
+++ Code2040/src/one/Challenge.java
@@ -1,5 +1,7 @@
 package one;
 
+import java.util.Arrays;
+
 /*
  
 What i want to do: 
@@ -29,7 +31,9 @@ What i want to do:
 
 
 public class Challenge {
-public static boolean isEvenTensPlace(int amount) {
+
+
+public static boolean isEvenTensPlace(int amount) { //this method determines whether the tens place/outputArr 1 is an odd #
 	
 	int rem = amount % 20;
 	
@@ -42,26 +46,95 @@ public static boolean isEvenTensPlace(int amount) {
 }
 	
 
-
-
-
-public static int numOfTwentyBills (int amount, int[] outputArr) {
+public static int numOfTwentyBills (int amount, int[] outputArr) { //This method is basically the cleanup for the amount, divding the rest or what given by 20
 		
 		
-		
-		
+		boolean isEven = isEvenTensPlace(amount);
+		if(isEven == true ) {
+			
+			outputArr[2] = amount / 20; 
+			
+		}
 		
 		return amount;
 		
 	}
-	
-	public static int [] withdraw(int amount) {
-		
-		
-		
-		return null;
-		
-	
-	}
 
+
+public static int numOfFiftyBills(int amount, int[] outputArr) {
+	boolean isEven = isEvenTensPlace(amount);  
+    int qout1 = amount / 50;
+    int remaider = amount % 50;
+    
+    if(isEven == false){
+      //outputArr[1] = someValue
+       
+      outputArr[1] = qout1;
+      
+       return 1;
+      
+    }else{
+      
+      return amount;
+    }
+
+}
+
+
+
+public static int numOfHundredBills(int amount, int[] outputArr) {
+	
+	int mod = amount % 100; 
+    int qout = amount / 100;
+    
+    outputArr[0] = qout;
+    
+		/*
+		 * if (outputArr [0] == 1) { System.out.println(qout); }else {
+		 * System.out.print(mod); }
+		 */
+    
+    return mod;
+}
+
+	
+	/*
+	 * public static char[] withdraw(int amount) { //this method is hopefully
+	 * turning the amount into a char array
+	 * 
+	 * char [] outputArr = String.valueOf(amount).toCharArray();
+	 * 
+	 * 
+	 * return outputArr ;
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
+	public static void main (String args[]) {
+		
+		int amount = 130;
+		int [] outputArr = {0,0,0};
+		
+		boolean isEven = isEvenTensPlace(amount);
+		System.out.println("is the tens place even for " + amount + ": " + isEven);
+		 
+	    System.out.println("amount: " + amount);
+	    System.out.println("outputArr: " + Arrays.toString(outputArr)); 
+	   
+		
+		
+	    int twenty = numOfTwentyBills(amount,outputArr);
+	    int fity = numOfFiftyBills(amount, outputArr);
+		int hundo = numOfHundredBills(amount, outputArr);
+	}
+	
+	
+	
 }
